5490a6c215bccc9c79170985b2993b9e14c9f729
SamsulAlomLaskar <itsmesamsulalom@gmail.com>
2025-08-24 17:03:42 +0530

    dynamic rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { cookies } from "next/headers";
 
 const AboutPage = () => {
   console.log("AboutPage component rendered");
+
+  const cookieStore = cookies();
+
+  const theme = cookieStore.get("theme");
 
   return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 };
